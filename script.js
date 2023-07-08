9d00200a46c4748a65e852c38d4d3d9879c536f4
Saeed Hashi <109211054+theonehash@users.noreply.github.com>
2023-07-08 20:50:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
 
+burger.addEventListener('mouseover', () => {
+  const lines = document.querySelectorAll('.line');
+  lines.forEach(line => {
+    line.style.backgroundColor = '#aaa';
+  });
+});
+
+burger.addEventListener('mouseout', () => {
+  const lines = document.querySelectorAll('.line');
+  lines.forEach(line => {
+    line.style.backgroundColor = '#fff';
+  });
+});
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('nav-active');
+  burger.classList.toggle('toggle');
+});
